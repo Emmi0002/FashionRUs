2b721c52400c31e1945c20f8dce3a8fe486b0a68
Emilie <emmi0002@stud.kea.dk>
2025-02-06 10:11:58 +0100

    done med discountlabel og sold out label

--- js/product.js
+++ js/product.js
@@ -1,17 +1,23 @@
 console.log("prøve..");
 
-let productId = 1545;
+// let productId = 1545;
 let productContainer = document.querySelector(".product_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+const myProduct = new URLSearchParams(window.location.search).get("id");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${myProduct}`)
   .then((response) => response.json())
   .then((data) => {
     productContainer.innerHTML = `
      <main class="gridProdukt_1-1">
-      <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="sko" />
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${myProduct}.webp" alt="product image" />
+      ${data.soldout > 0 ? `<div class="SoldOut_product">Sold Out</div>` : ""}
       <div>
         <h3>${data.productdisplayname}</h3>
-        <p>${data.price} DKK</p>
+        ${data.discount > 0 ? `<span class="old_price"> ${data.price} DKK</span><br />` : ""}
+        ${data.discount < 0 ? `<p class="old_price">${data.price} DKK</p><br />` : ""}   
+        <p class="new_price">${Math.round(data.price * (1 - data.discount / 100))} DKK</p><br />
+        ${data.discount > 0 ? `<p class="discountlabel_product"> ${data.discount} %</p><br />` : ""}
         <p>Color: ${data.basecolour}</p>
         <p class="size">Size</p>
         <section class="grid_1-1-1">
@@ -22,7 +28,7 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
           <p>XL</p>
           <p>XXL</p>
         </section>
-        <p class="InventoryNr">Inventory Number: ${productId}</p>
+        <p class="InventoryNr">Inventory Number: ${myProduct}</p>
         <p class="AddToBasket">ADD TO BASKET</p>
       </div>
     </main>`;
--- js/productlist.js
+++ js/productlist.js
@@ -1,6 +1,6 @@
 const listContainer = document.querySelector(".product_list_container");
 
-myCategory = new URLSearchParams(window.location.search).get("category");
+const myCategory = new URLSearchParams(window.location.search).get("category");
 
 fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`)
   .then((response) => response.json())
@@ -15,13 +15,16 @@ function showList(products) {
     .map((product) => {
       markup += `
         <section>
-          <a href="produkt.html"><img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="t-shirt" /></a>
+          <a href="produkt.html?id=${product.id}"><img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="t-shirt" /></a>
+          ${product.soldout > 0 ? `<div class="SoldOut">Sold Out</div>` : ""}
           <h3>${product.productdisplayname}</h3>
           <div class="grid_1-1">
             <p>${product.articletype} | ${product.brandname}</p>
             <section><p>Read More</p></section>
           </div>
-          <p>${product.price} DKK</p>
+          ${product.discount > 0 ? `<span class="old_price"> ${product.price} DKK</span><br />` : ""} 
+          <span class="new_price"> ${Math.round(product.price * (1 - product.discount / 100))} DKK</span><br/>
+          ${product.discount > 0 ? `<p class="discountlabel_2"> ${product.discount} %</p><br />` : ""} 
         </section>`;
     })
     .join("");
